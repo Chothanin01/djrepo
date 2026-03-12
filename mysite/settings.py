108aea9ccb8801c59133855406e9f2ff39886196
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2026-03-12 23:03:24 +0700

    new build

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+        conn_max_age=600
+    ),
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'), # 'resume_db'
