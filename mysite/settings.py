85fc778b24b69147ed065a335ea6d3ecbee57b24
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2026-03-12 23:21:35 +0700

    update get env

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL")
     ),
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
